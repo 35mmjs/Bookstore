75cbf0ed0e354d6655d7a8f7a8e86306a846a846
六娃 <wencheng.lwc@alibaba-inc.com>
2018-12-15 16:01:04 +0800

    feat: bookAPI

--- app/service/bookAPI.js
+++ app/service/bookAPI.js
@@ -20,7 +20,7 @@ class BookAPIService extends Service {
       khid: bookConfig.khid,
       md5_value: md5(`${bookConfig.khid}${bookConfig.keyid}${d.getFullYear()}${d.getMonth() + 1}`),
       par_type: 'json',
-      par_value: JSON.stringify({ params: data }),
+      par_value: JSON.stringify(data),
     }
     return new Promise((resolve, reject) => {
       soap.createClient(bookConfig.url, (err, client) => {
@@ -33,11 +33,11 @@ class BookAPIService extends Service {
             reject(err2)
             return
           }
-          const ret = result.sv_serviceReturn.$value
-          if (ret.code !== '200') {
-            reject(ret)
+          const ret = JSON.parse(result.sv_serviceReturn.$value)
+          if (ret.code === '200' || ret.code === '000') {
+            resolve(ret.data)
           } else {
-            resolve(ret)
+            reject(ret)
           }
         })
       })
@@ -45,10 +45,46 @@ class BookAPIService extends Service {
   }
 
   /**
-   * 排行榜查询
+   * 获取排行榜分类列表
+   * @param khbh {String} - 客户编号，如果不传入则代表使用全省通用排行, 如果传入则代表单店排行
+   * @return {Array}
+   *  - phid {String} 排行ID
+   *  - phmc {phmc} 排行名称
    */
-  getRinkingInfo() {
-    return this.fetch('GetRinkingInfo', { khbh: '3300000000', '1x': 'list' })
+  getRinkingList(khbh) {
+    return this.fetch('GetRinkingInfo', { khbh: khbh || '3300000000', lx: 'list' })
+  }
+
+  /**
+   * 获取排行分类的详细信息
+   * @param phid {String} - 排行ID，由getRinkingList列表获取
+   * @param khbh {String} - 客户编号，如果不传入则代表使用全省通用排行, 如果传入则代表单店排行
+   * @return {Array}
+   *  - phid 排行id
+   *  - phmc 排行名称
+   *  - xh 排行序号
+   *  - tm 条码/ISBN
+   *  - spbs 商品标识
+   *  - sm 书名
+   *  - dj 定价
+   */
+  getRinkingInfo(phid, khbh) {
+    return this.fetch('GetRinkingInfo', { khbh: khbh || '3300000000', phid, lx: 'detailed' })
+  }
+
+  /**
+   * 根据书号获取书的详细信息
+   * @param ISBN {String} - 书号
+   * @return {Object}
+   *  - spbs 浙江新华图书商品的唯一标识
+   *  - sm 书名
+   *  - di 定价
+   *  - isbn 书号
+   *  - zyz 作者
+   *  - fmtp 封面图片
+   */
+  searchBookByISBN(ISBN) {
+    return this.fetch('itemInfoSearch', { params: { type: 'ISBN', value: ISBN } }).then(d => JSON.parse(d)[0])
   }
 }
 
--- test/app/service/bookAPI.test.js
+++ test/app/service/bookAPI.test.js
@@ -1,8 +1,21 @@
 const { app } = require('egg-mock/bootstrap')
+const expect = require('expect')
 
 describe('test/app/service/bookAPI.test.js', () => {
-  it('fetch', async () => {
-    const ctx = app.mockContext({})
-    await ctx.service.bookAPI.getRinkingInfo()
+  let ctx
+  beforeEach(() => {
+    ctx = app.mockContext({})
+  })
+  it('getRinkingList', async () => {
+    const data = await ctx.service.bookAPI.getRinkingList()
+    expect(Object.keys(data[0])).toEqual(['phid', 'phmc'])
+  })
+  it('getRinkingInfo', async () => {
+    const data = await ctx.service.bookAPI.getRinkingInfo('00000084')
+    expect(Array.isArray(data)).toEqual(true)
+  })
+  it('searchBookByISBN', async () => {
+    const data = await ctx.service.bookAPI.searchBookByISBN('9787509392652')
+    expect(data.sm).toMatch(/宪法/)
   })
 })
