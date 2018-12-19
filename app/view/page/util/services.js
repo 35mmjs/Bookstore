e53e6cc08d04e983961ac0c8f1a037529bcda013
pizn <pizner@gmail.com>
2018-12-19 20:54:20 +0800

    feat: udpate

--- app/view/page/util/services.js
+++ app/view/page/util/services.js
@@ -1,17 +1,16 @@
-import ajax from '../../common/ajax'
+import axios from 'axios'
 
 export const search = (data) => {
-  return ajax({
+  return axios({
     url: 'http://47.96.75.202/open/v1book/search',
     data,
     method: 'post',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   })
 }
 
 export const getZhantaiData = (data) => {
-  return ajax({
-    url: '/open/v1/zhantai',
-    data,
-    method: 'get',
-  })
+  return axios.get('http://47.96.75.202/open/v1/zhantai')
 }
--- app/view/page/zhantai/App/index.js
+++ app/view/page/zhantai/App/index.js
@@ -1,51 +1,111 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Slider from 'react-slick'
 import { getZhantaiData } from '../../util/services'
 import Single from '../Single'
-import books from '../data'
+import data from '../data'
 import './index.less'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      handUp: false,
+      books: data,
     }
+
+    this.timeout = null
   }
 
   componentDidMount() {
     this.getData()
+    const that = this;
+    window.handleBookEvent = function (isbns) {
+      that.handleBookEvent(isbns)
+    }
   }
 
-  getData = () => {
-    getZhantaiData({
-      clientId: '1111',
-    }).then(res => {
-      console.log(res)
-    }).catch(err => {
-      console.error(err)
+  handleBookEvent = (isbns) => {
+    this.pause()
+    this.setState({
+      handUp: true,
+      books: data.slice(0, isbns.length),
+    }, () => {
+      this.autoPlay()
     })
   }
 
-  render() {
-    const width = books.length > 1 ? 956 * books.length + 'px' : '100%'
-    const wrapStyle = {
-      width,
+  autoPlay = () => {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
     }
+
+    this.timeout = setTimeout(() => {
+      this.play()
+      this.setState({
+        handUp: false,
+        books: data,
+      })
+    }, 5000)
+  }
+
+  getData = () => {
+    getZhantaiData({ client: '1' })
+      .then(res => {
+        if (res.data.success) {
+          console.log(res.data.data.books)
+          this.setState({
+            books: res.data.data.books,
+            handUp: false,
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  play = () => {
+    this.slider.slickPlay()
+  }
+
+  pause = () => {
+    this.slider.slickPause()
+  }
+
+  reload = () => {
+    window.location.reload()
+  }
+
+  render() {
+    const { books, handUp } = this.state
+    const settings = {
+      infinite: true,
+      slidesToShow: handUp && books.length > 1 ? 2 : 1,
+      slidesToScroll: handUp && books.length > 1 ? 2 : 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      variableWidth: true,
+    }
+
+    console.log('-->', books)
     return (
       <div className="app">
-        <div className="warpper" style={wrapStyle}>
-          {
-            books.length === 1 && (
-              <Single book={books[0]} theme="lightBlue" />
-            )
-          }
-          { books.length > 1 && books.map((book, index) => {
-            const theme = index % 2 === 0 ? 'lightBlue' : ''
-            return (
-              <Single book={book} theme={theme} mulity key={book.isbn} />
-            )
-          })
-          }
+        <div className="warpper">
+          <Slider ref={slider => (this.slider = slider)} {...settings}>
+            {books.map((book, index) => {
+              const theme = index % 2 === 0 ? 'lightBlue' : ''
+              return (
+                <Single book={book} theme={theme} mulity={handUp && books.length > 1} key={book.id} />
+              )
+            })}
+          </Slider>
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          <button className="button" onClick={this.play}>Play</button>
+          <button className="button" onClick={this.pause}>Pause</button>
+          <button className="button" onClick={this.reload}>Reload</button>
         </div>
       </div>
     )
