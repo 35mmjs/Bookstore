9148e2a50787fad1aa6419b387928813ec42dcd5
pizn <pizner@gmail.com>
2018-12-20 08:14:04 +0800

    feat: support manifest

--- app/view/page/util/services.js
+++ app/view/page/util/services.js
@@ -1,14 +1,7 @@
 import axios from 'axios'
 
 export const search = (data) => {
-  return axios({
-    url: 'http://47.96.75.202/open/v1book/search',
-    data,
-    method: 'post',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  })
+  return axios.get('http://47.96.75.202/open/v1/book/search')
 }
 
 export const getPubuData = (data) => {
