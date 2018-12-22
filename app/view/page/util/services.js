af838db2bc1f2e37989e7d2ba2e0dc213ca5bbeb
pizn <pizner@gmail.com>
2018-12-22 08:42:48 +0800

    feat: pubu 增加数据获取

--- app/view/page/util/services.js
+++ app/view/page/util/services.js
@@ -8,6 +8,14 @@ export const getPubuData = (data) => {
   return axios.get('http://47.96.75.202/open/v1/pubu')
 }
 
+export const getRecommend = (data) => {
+  return axios.get(`http://47.96.75.202/open/v1/book/recommend?isbn=${data.isbn}`)
+}
+
 export const getZhantaiData = (data) => {
   return axios.get('http://47.96.75.202/open/v1/zhantai')
+}
+
+export const getBook = (data) => {
+  return axios.get(`http://47.96.75.202/open/v1/book?isbn=${data.isbn}`)
 }
