13de42218fa8602f2c4ca958507de65838fef3bf
pizn <pizner@gmail.com>
2019-03-02 20:27:41 +0800

    feat: return cover

--- app/view/page/paihangpad/App/index.js
+++ app/view/page/paihangpad/App/index.js
@@ -7,8 +7,6 @@ import './index.less'
 
 const book = {"cover":"https://bookstore-public.oss-cn-hangzhou.aliyuncs.com/book-cover.gif","isbn":"9787534184505","spbs":"4350571","score": "9.2","name":"2019年<己亥年>历书","author":"栈新","catalog":"天 文 学","toc":"来(21种正在改↵变世界的神奇科技)》植根《我是未来》节目，但不↵局限于《我是未来》节目，本书集结全球顶尖科技咖↵对全世界最前沿且与人类生活息息相关的21项科技进↵行汇总和分享，全景勾勒智慧时代的未来生活。↵","price":"3","pricing":"3","recommender":"","intro":"    我是未来节目组组编的《遇见未来(21种正在改↵变世界的神奇科技)》植根《我是未来》节目，但不↵局限于《我是未来》节目，本书集结全球顶尖科技咖↵对全世界最前沿且与人类生活息息相关的21项科技进↵行汇总和分享，全景勾勒智慧时代的未来生活。↵","pageType":"64开","pageNum":"64","publish":"浙江科学技术出版社","version":"","bookshelf":"","stockList":[],"qrcode":"http://wap.zxhsd.com/index/item.shtml?spbs=4350571&khbh=3300000000"}
 
-console.log(book)
-
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -26,11 +24,11 @@ class App extends React.Component {
     const that = this
     this.timeout = setTimeout(() => {
       getPaihangDetail().then(res => {
-        console.log(res)
         const { data } = res
         if (data.success) {
           that.setState({
             book: data.data,
+            showDetail: false,
           })
         }
         this.getData()
