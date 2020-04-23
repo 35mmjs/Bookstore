4044a1d5257c41488a234e208f58ca9dfc9cf91e
夏子飞 <xiazifei1987@163.com>
2020-04-23 16:28:37 +0800

    ditu

---
+++ app/view/page/daoshi/Map/10022.js
@@ -0,0 +1,321 @@
+{
+	"map": {
+		"src": "https://bookstore-public.oss-cn-hangzhou.aliyuncs.com/map/%E8%8D%B7%E5%9F%8E%E4%B9%A6%E8%8B%91%E5%9C%B0%E5%9B%BE.png",
+		"size": [22.78, 23.88],
+		"floorCount": 1
+	},
+	"type": "normal",
+	"floor": [{
+			"key": "综合",
+			"floor": [{
+					"key": 1,
+					"name": "",
+					"areas": [{
+						"key": "s",
+						"name": "舞台",
+						"color": "#f5f5f5",
+						"stockList": [1, 6],
+						"coordinate": [3.79, 19.69]
+					}],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 2,
+					"name": "",
+					"areas": [{
+						"key": "a",
+						"name": "收银台",
+						"color": "#3cb371",
+						"stockList": [1, 2],
+						"coordinate": [14.46, 2.13]
+					}],
+					"catalog": [],
+					"location": []
+				}
+			]
+		},
+		{
+			"key": "文学艺术",
+			"floor": [{
+					"key": 1,
+					"name": "",
+					"areas": [{
+							"key": "d1",
+							"name": "青春文学",
+							"color": "#8fbc8f",
+							"stockList": [1, 2],
+							"coordinate": [10.60, 3.48]
+						},
+						{
+							"key": "d2",
+							"name": "悬疑幻想",
+							"color": "#8fbc8f",
+							"stockList": [3],
+							"coordinate": [9.58, 4.50]
+						},
+						{
+							"key": "d2",
+							"name": "悬疑幻想",
+							"color": "#8fbc8f",
+							"stockList": [
+								4, 6
+							],
+							"coordinate": [9.58, 4.50]
+						}
+					],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 2,
+					"name": "",
+					"areas": [{
+							"key": "d4",
+							"name": "中国文学",
+							"color": "#8fbc8f",
+							"stockList": [
+								1, 6
+							],
+							"coordinate": [11.92, 4.63]
+						},
+						{
+							"key": "g",
+							"name": "诗词散文",
+							"color": "#8fbc8f",
+							"stockList": [
+								7, 11
+							],
+							"coordinate": [8.43, 7.78]
+						}
+					],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 3,
+					"name": "",
+					"areas": [{
+						"key": "i",
+						"name": "古典文学",
+						"color": "#d2b48c",
+						"stockList": [1, 2],
+						"coordinate": [7.79, 9.91]
+					}],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 4,
+					"name": "",
+					"areas": [{
+						"key": "q",
+						"name": "主题展台",
+						"color": "#4169e1",
+						"stockList": [
+							1, 4
+						],
+						"coordinate": [10.93, 16.51]
+					}],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 5,
+					"name": "",
+					"areas": [{
+						"key": "s",
+						"name": "舞台",
+						"color": "#f5f5f5",
+						"stockList": [
+							1, 4
+						],
+						"coordinate": [3.79, 19.69]
+					}],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 6,
+					"name": "",
+					"areas": [{
+						"key": "b",
+						"name": "新书榜、畅销榜",
+						"color": "#ffe4e1",
+						"stockList": [1, 2],
+						"coordinate": [14.63, 3.28]
+					}],
+					"catalog": [],
+					"location": []
+				}
+			]
+		},
+		{
+			"key": "社会科学",
+			"floor": [{
+					"key": 1,
+					"name": "",
+					"areas": [{
+							"key": "m",
+							"name": "法律",
+							"color": "#d2b48c",
+							"stockList": [1],
+							"coordinate": [7.45, 14.11]
+						},
+						{
+							"key": "n",
+							"name": "历史传记",
+							"color": "#d2b48c",
+							"stockList": [2],
+							"coordinate": [5.62, 15.66]
+						},
+						{
+							"key": "o",
+							"name": "哲学励志",
+							"color": "#d2b48c",
+							"stockList": [3],
+							"coordinate": [3.86, 17.19]
+						}
+					],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 2,
+					"name": "",
+					"areas": [{
+							"key": "m",
+							"name": "法律",
+							"color": "#d2b48c",
+							"stockList": [1],
+							"coordinate": [7.45, 14.11]
+						},
+						{
+							"key": "n",
+							"name": "历史传记",
+							"color": "#d2b48c",
+							"stockList": [2],
+							"coordinate": [5.62, 15.66]
+						},
+						{
+							"key": "o",
+							"name": "哲学励志",
+							"color": "#d2b48c",
+							"stockList": [3],
+							"coordinate": [3.86, 17.19]
+						}
+					],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 3,
+					"name": "",
+					"areas": [{
+							"key": "u",
+							"name": "党政",
+							"color": "#ff0000",
+							"stockList": [1, 2],
+							"coordinate": [15.71, 16.88]
+						},
+						{
+							"key": "v",
+							"name": "经济",
+							"color": "#f0e68c",
+							"stockList": [3],
+							"coordinate": [13.71, 18.67]
+						}
+					],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 4,
+					"name": "",
+					"areas": [{
+							"key": "e",
+							"name": "智能展台",
+							"color": "#8fbc8f",
+							"stockList": [1],
+							"coordinate": [14.73, 4.67]
+						},
+						{
+							"key": "f",
+							"name": "文学平台",
+							"color": "#8fbc8f",
+							"stockList": [2],
+							"coordinate": [12.49, 6.8]
+						}
+					],
+					"catalog": [],
+					"location": []
+				},
+				{
+					"key": 5,
+					"name": "",
+					"areas": [{
+						"key": "p",
+						"name": "桂板书",
+						"color": "#b0c4de",
+						"stockList": [1, 2, 3],
+						"coordinate": [11.88, 14.14]
+					}],
+					"catalog": [],
+					"location": []
+				}
+			]
+		},
+		{
+			"key": "文化教育",
+			"floor": [{
+				"key": 1,
+				"name": "",
+				"areas": [{
+					"key": "t",
+					"name": "成教字典",
+					"color": "#b0c4de",
+					"stockList": [
+						1, 2
+					],
+					"coordinate": [18.11, 15.12]
+				}],
+				"catalog": [],
+				"location": []
+			}]
+		},
+		{
+			"key": "科学技术",
+			"floor": [{
+				"key": 1,
+				"name": "",
+				"areas": [{
+					"key": "w",
+					"name": "科技科普",
+					"color": "#f0e68c",
+					"stockList": [1, 2],
+					"coordinate": [11.92, 20.4]
+				}],
+				"catalog": [],
+				"location": []
+			}]
+		},
+		{
+			"key": "少儿读物",
+			"floor": [{
+				"key": 1,
+				"name": "",
+				"areas": [{
+					"key": "k",
+					"name": "少儿",
+					"color": "#f0e68c",
+					"stockList": [
+						1, 8
+					],
+					"coordinate": [12.59, 22.40]
+				}],
+				"catalog": [],
+				"location": []
+			}]
+		}
+	]
+}
